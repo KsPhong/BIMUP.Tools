614dc35c5895a854bef7109be2e01736a81e53f0
Ks.Phong <91179896+KsPhong@users.noreply.github.com>
2025-06-12 22:48:39 +0700

    Add project files.

---
+++ BIMUP.API/Geometry.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Geometry;
+
+public static class GeometryUtilities
+{
+    #region Constants
+
+    private const double DEFAULT_TOLERANCE = 1e-6;
+
+    #endregion Constants
+}
+
+
+public class GeoPlane
+{
+    #region Private Properties
+    private Vector3d _normal;
+    private Point3d _origin;
+    private Vector3d _xAxis;
+    private Vector3d _yAxis;
+    #endregion
+
+    #region Public Properties
+    public Point3d Origin
+    {
+        get { return _origin; }
+        set { _origin = value; }
+    }
+    public Vector3d XAxis
+    {
+        get { return _xAxis; }  
+        set { _xAxis = value; }
+    }
+
+    public Vector3d YAxis
+    {
+        get { return _yAxis; }
+        set { _yAxis = value; }
+    }
+
+    public Vector3d Normal
+    {
+        get { return _normal; }
+        set { _normal = value; }
+    }
+    #endregion
+
+    #region Construction
+
+    public GeoPlane(Point3d origin,Vector3d normal)
+    {
+        _origin = origin;
+        _normal = normal;
+    }
+
+    #endregion
+}
+
+public class GeoCoordinateSystem
+{
+    #region Private Properties
+    private Point3d _origin;
+
+    private Vector3d _xAxis;
+    private Vector3d _yAxis;
+    private Vector3d _zAxis;
+
+    #endregion
+
+    #region Public Properties
+    public Point3d Origin { get { return _origin; } }
+    public Vector3d XAxis { get { return _xAxis; } }
+    public Vector3d YAxis { get { return _yAxis; } }
+    public Vector3d ZAxis { get { return _zAxis; } }
+    #endregion
+
+    #region Internal Constructor (Hàm khởi tạo nội bộ)
+    /// <summary>
+    /// Hàm khởi tạo chính, nhận đầy đủ thông tin. 
+    /// Các hàm khác sẽ gọi hàm này sau khi đã tính toán xong.
+    /// </summary>
+    private GeoCoordinateSystem(Point3d origin, Vector3d xAxis, Vector3d yAxis, Vector3d zAxis)
+    {
+        _origin = origin;
+        // Đảm bảo các vector trục luôn là vector đơn vị (chiều dài = 1)
+        _xAxis = xAxis.GetNormal();
+        _yAxis = yAxis.GetNormal();
+        _zAxis = zAxis.GetNormal();
+    }
+    #endregion
+
+    #region Identity (Hệ tọa độ gốc WCS)
+    // Tương ứng với node "Identity"
+
+    /// <summary>
+    /// Tạo một hệ tọa độ trùng với Hệ tọa độ Thế giới (World Coordinate System - WCS).
+    /// </summary>
+    public GeoCoordinateSystem()
+    {
+        _origin = Point3d.Origin; // Gốc tại (0,0,0)
+        _xAxis = Vector3d.XAxis;  // (1,0,0)
+        _yAxis = Vector3d.YAxis;  // (0,1,0)
+        _zAxis = Vector3d.ZAxis;  // (0,0,1)
+    }
+
+    /// <summary>
+    /// Một thuộc tính tĩnh (static) để lấy nhanh hệ tọa độ WCS mà không cần tạo đối tượng mới.
+    /// </summary>
+    public static GeoCoordinateSystem WCS
+    {
+        get { return new GeoCoordinateSystem(); }
+    }
+    #endregion
+
+    #region Construction
+    public GeoCoordinateSystem(double x, double y) : this()
+    {
+        _origin = new Point3d(x, y, 0);
+    }
+    public GeoCoordinateSystem(double x, double y,double z): this()
+    {
+        _origin = new Point3d(x, y, z);
+    }
+
+    #endregion
+}
+
---
+++ BIMUP.Tools/Orther.cs
@@ -0,0 +1,212 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace BIMUP.Core
+{
+    public static class Orther
+    {
+        [CommandMethod("EXPORTSELECTEDTEXT")]
+        public static void ExportSelectedText()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+
+            // Yêu cầu người dùng chọn đối tượng
+            PromptSelectionResult psr = ed.GetSelection();
+
+            if (psr.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nThao tác chọn đã bị hủy. Không có văn bản nào được xuất.");
+                return;
+            }
+
+            SelectionSet ss = psr.Value;
+
+            if (ss.Count == 0)
+            {
+                ed.WriteMessage("\nKhông có đối tượng nào được chọn. Không có văn bản nào được xuất.");
+                return;
+            }
+
+            // Mở hộp thoại lưu file
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Chọn nơi lưu file Text từ các đối tượng đã chọn";
+            saveFileDialog.FileName = "ExportedSelectedText.txt"; // Tên file mặc định
+
+            if (saveFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                ed.WriteMessage("\nThao tác đã bị hủy bởi người dùng.");
+                return;
+            }
+
+            string filePath = saveFileDialog.FileName;
+            StringBuilder sb = new StringBuilder();
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                foreach (SelectedObject acObj in ss)
+                {
+                    if (acObj == null || acObj.ObjectId.IsNull)
+                        continue;
+
+                    Entity ent = tr.GetObject(acObj.ObjectId, OpenMode.ForRead) as Entity;
+
+                    if (ent is DBText)
+                    {
+                        DBText text = ent as DBText;
+                        sb.AppendLine(text.TextString);
+                    }
+                    else if (ent is MText)
+                    {
+                        MText mtext = ent as MText;
+                        sb.AppendLine(mtext.Contents);
+                    }
+                    else if (ent is AttributeReference)
+                    {
+                        AttributeReference attRef = ent as AttributeReference;
+                        sb.AppendLine(attRef.TextString);
+                    }
+                    else if (ent is BlockReference)
+                    {
+                        BlockReference br = ent as BlockReference;
+                        Autodesk.AutoCAD.DatabaseServices.AttributeCollection attCol = br.AttributeCollection;
+                        foreach (ObjectId attId in attCol)
+                        {
+                            AttributeReference attRef = tr.GetObject(attId, OpenMode.ForRead) as AttributeReference;
+                            if (attRef != null)
+                            {
+                                sb.AppendLine(attRef.TextString);
+                            }
+                        }
+                    }
+                    else if (ent is AttributeDefinition)
+                    {
+                        AttributeDefinition attDef = ent as AttributeDefinition;
+                        sb.AppendLine(attDef.TextString);
+                    }
+                }
+                tr.Commit();
+            }
+
+            try
+            {
+                File.WriteAllText(filePath, sb.ToString(), Encoding.UTF8);
+                ed.WriteMessage($"\nĐã xuất văn bản từ các đối tượng đã chọn thành công ra: {filePath}");
+            }
+            catch (System.Exception ex)
+            {
+                ed.WriteMessage($"\nLỗi khi xuất văn bản từ các đối tượng đã chọn: {ex.Message}");
+            }
+        }
+
+
+        [CommandMethod("COPYTEXTTOCLIPBOARD")]
+        public static void CopyToClipboard()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+
+            // Yêu cầu người dùng chọn đối tượng
+            PromptSelectionResult psr = ed.GetSelection();
+
+            if (psr.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nThao tác chọn đã bị hủy. Không có văn bản nào được sao chép.");
+                return;
+            }
+
+            SelectionSet ss = psr.Value;
+
+            if (ss.Count == 0)
+            {
+                ed.WriteMessage("\nKhông có đối tượng nào được chọn. Không có văn bản nào được sao chép.");
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                foreach (SelectedObject acObj in ss)
+                {
+                    if (acObj == null || acObj.ObjectId.IsNull)
+                        continue;
+
+                    Entity ent = tr.GetObject(acObj.ObjectId, OpenMode.ForRead) as Entity;
+
+                    if (ent is DBText)
+                    {
+                        DBText text = ent as DBText;
+                        sb.AppendLine(text.TextString);
+                    }
+                    else if (ent is MText)
+                    {
+                        MText mtext = ent as MText;
+                        sb.AppendLine(mtext.Contents);
+                    }
+                    else if (ent is AttributeReference)
+                    {
+                        AttributeReference attRef = ent as AttributeReference;
+                        sb.AppendLine(attRef.TextString);
+                    }
+                    else if (ent is BlockReference)
+                    {
+                        BlockReference br = ent as BlockReference;
+                        Autodesk.AutoCAD.DatabaseServices.AttributeCollection attCol = br.AttributeCollection;
+                        foreach (ObjectId attId in attCol)
+                        {
+                            AttributeReference attRef = tr.GetObject(attId, OpenMode.ForRead) as AttributeReference;
+                            if (attRef != null)
+                            {
+                                sb.AppendLine(attRef.TextString);
+                            }
+                        }
+                    }
+                    else if (ent is AttributeDefinition)
+                    {
+                        AttributeDefinition attDef = ent as AttributeDefinition;
+                        sb.AppendLine(attDef.TextString);
+                    }
+                }
+                tr.Commit();
+            }
+
+            try
+            {
+                // Copy the collected text to the clipboard
+                if (sb.Length > 0)
+                {
+                    // The Clipboard class requires a STA thread. 
+                    // Since AutoCAD commands run on an MTA thread, we need to use a separate thread or invoke.
+                    // For simplicity in a direct AutoCAD command, we'll try direct access,
+                    // but for robust applications, consider Application.Invoke or a dedicated STA thread.
+
+                    // A more robust way to handle STA apartment requirement for Clipboard:
+                    System.Threading.Thread staThread = new System.Threading.Thread(() => Clipboard.SetText(sb.ToString()));
+                    staThread.SetApartmentState(System.Threading.ApartmentState.STA);
+                    staThread.Start();
+                    staThread.Join(); // Wait for the thread to complete
+
+                    ed.WriteMessage("\nĐã sao chép văn bản từ các đối tượng đã chọn vào Clipboard thành công.");
+                }
+                else
+                {
+                    ed.WriteMessage("\nKhông tìm thấy văn bản nào trong các đối tượng đã chọn để sao chép.");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                ed.WriteMessage($"\nLỗi khi sao chép văn bản vào Clipboard: {ex.Message}");
+            }
+        }
+    }
+}
